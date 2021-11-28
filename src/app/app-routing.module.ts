22b16360a0e61e56c8bebfa498294c4befb134ab
unknown <edison.alfonso.casas@gmail.com>
2021-11-28 13:55:53 -0500

    Ajuste de scripts faltantes y creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InsertarIngresosComponent } from './components/insertar-ingresos/insertar-ingresos.component';
+import { InsertarGastosComponent } from './components/insertar-gastos/insertar-gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'gastos', component: InsertarGastosComponent},
+  {path: 'ingresos', component: InsertarIngresosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
